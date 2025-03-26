5ca6ba07b207c8b7083e141359730d872aea94c8
polyerm <pa.yermolenko@gmail.com>
2025-03-26 22:48:36 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-/* public class Main {
+/*
+public class Main {
     public static void main(String[] args) {
 // Create an instance of the computer store
         ComputerStore store = new ComputerStore();
